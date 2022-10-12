21d75e799227e4bf762c5ddd7293fe528eaee11c
Ollie Cross <iio.olivercross@gmail.com>
2022-10-12 20:17:40 +0100

    Added a get request route. Added data to a JSON file

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const cors = require("cors");
+const crypto = require("crypto");
+// const { v4: uuid } = require("uuid");
+const fs = require("fs");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//DOTENV
+require("dotenv").config();
+const { PORT } = process.env;
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+//TEST
+app.use((req, res, next) => {
+  console.log(
+    `Incoming request: ${req.path}, Host: ${req.hostname} / IP: ${req.ip}`
+  );
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+//GET DATA
+app.route("/videos").get((req, res) => {
+  const fileContent = fs.readFileSync("./data/videos.json");
+  res.status(200).json(JSON.parse(fileContent));
+});
+// .post((req, res) => {
+//   console.log(/*data*/);
+//   res.status(201).json(/*data*/);
+// });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
