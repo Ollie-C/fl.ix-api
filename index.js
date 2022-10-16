f0cf6e0cd987bc9a90a75c3dc4cc6cd0749a3b5a
Ollie Cross <iio.olivercross@gmail.com>
2022-10-16 15:18:39 +0100

    Added a GET route to return comments for an individual video and a POST route to add a comment

--- index.js
+++ index.js
@@ -36,25 +36,25 @@ app
       channel: "Red Cow",
       image: "https://i.imgur.com/l2Xfgpl.jpg",
       description: req.body.description,
-      views: "1",
-      likes: "1",
+      views: Math.floor(Math.random() * 10000),
+      likes: Math.floor(Math.random() * 100),
       duration: "3:00",
       video: "https://project-2-api.herokuapp.com/stream",
-      timestamp: req.body.timestamp,
+      timestamp: Date.now(),
       comments: [
         {
           id: crypto.randomBytes(16).toString("hex"),
           name: "Nice",
           comment: "Nice.",
           likes: 100,
-          timestamp: req.body.timestamp,
+          timestamp: Date.now(),
         },
         {
           id: crypto.randomBytes(16).toString("hex"),
           name: "Nice",
           comment: "Nice.",
           likes: 10000,
-          timestamp: req.body.timestamp,
+          timestamp: Date.now(),
         },
       ],
     };
@@ -70,6 +70,31 @@ app.get("/:videosId", (req, res) => {
   );
   res.status(200).json(individualVideo);
 });
+
+app
+  .get("/:videosId/comments", (req, res) => {
+    const videosData = JSON.parse(fs.readFileSync("./data/videos.json"));
+    const individualVideoComments = videosData.find(
+      (video) => video.id === req.params.videosId
+    ).comments;
+    res.status(200).json(individualVideoComments);
+  })
+  .post("/:videosId/comments", (req, res) => {
+    const videosData = JSON.parse(fs.readFileSync("./data/videos.json"));
+    const newComment = {
+      id: crypto.randomBytes(16).toString("hex"),
+      name: "User Name",
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam sapiente, ab harum accusantium perferendis vero nesciunt saepe veniam unde sunt?",
+      likes: Math.floor(Math.random() * 1000),
+      timestamp: Date.now(),
+    };
+    videosData
+      .find((video) => video.id === req.params.videosId)
+      .comments.push(newComment);
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videosData));
+    res.status(201).json(newComment);
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
